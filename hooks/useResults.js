6fc9ea9cb1a7b78e4fbc7028078aabb6317de9c4
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-10-23 15:54:25 +0300

    end

--- hooks/useResults.js
+++ hooks/useResults.js
@@ -5,21 +5,33 @@ import yelp from "../api/yelp";
 // Arama yapma
 export default ()=>{
     const [results, setResults] = useState([])
-
+    const [errorMessage, setErrorMessage] = useState("")
+    try {
+        
+    } catch (error) {
+        
+    }
     const searchApi = async(searchTerm) => {
-        const response= await yelp.get('/search' , {
+        // hata yakalama
+        try {
+             const response= await yelp.get('/search' , {
             params :{
                 limit: 50 ,
                 term: searchTerm ,
                 location: 'İstanbul',
-            }
+            },
         })
         setResults(response.data.businesses);
+        setErrorMessage('')
+        } catch (error) {
+            setErrorMessage('Bağlantı Hatası')
+        }
+       
     };
     useEffect(() =>{
         searchApi('Toast');
     },[]);
 
-    return [searchApi ,results]; // api dışarı açma 
+    return [searchApi ,results , errorMessage]; // api dışarı açma 
 
 }
